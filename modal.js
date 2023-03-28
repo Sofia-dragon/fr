2c451abe9503e38f48c1d3f97b0ab3401c13478f
Sofia-dragon <120542763+Sofia-dragon@users.noreply.github.com>
2023-03-28 18:56:34 +0300

    28

--- modal.js
+++ modal.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-    $('.div-svg-menu').click(function (event) {
-        $('.svg-menu,.nav').toggleClass('active');
-    });
-});
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]")
+    };
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
